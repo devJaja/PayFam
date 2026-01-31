4cbe3232b58dc0dd899742dcaa5ca84dd0e32255
Dev Jaja <jajak5242@gmail.com>
2026-01-30 23:24:12 -0500

    docs: add subscription FAQ

--- smart-contracts/contracts/CommitHistory.sol
+++ smart-contracts/contracts/CommitHistory.sol
@@ -280,3 +280,4 @@ contract CommitHistory {
 // Commit 265: feat: add subscription reconciliation
 // Commit 266: security: implement subscription blacklist
 // Commit 267: refactor: optimize subscription caching
+// Commit 268: docs: add subscription FAQ
