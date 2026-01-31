7a9d8eabf33bde5399670cd211659f612617b4c6
Dev Jaja <jajak5242@gmail.com>
2026-01-30 23:24:14 -0500

    security: add dispute auditing

--- smart-contracts/contracts/CommitHistory.sol
+++ smart-contracts/contracts/CommitHistory.sol
@@ -304,3 +304,4 @@ contract CommitHistory {
 // Commit 289: refactor: optimize dispute updates
 // Commit 290: docs: add dispute best practices
 // Commit 291: feat: implement dispute search
+// Commit 292: security: add dispute auditing
