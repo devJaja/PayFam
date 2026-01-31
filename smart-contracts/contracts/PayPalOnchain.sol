8cc7950b0df0039a353e4a0e2348cd02035fea48
Dev Jaja <jajak5242@gmail.com>
2026-01-30 23:13:01 -0500

    feat: add invoice template system

--- smart-contracts/contracts/PayPalOnchain.sol
+++ smart-contracts/contracts/PayPalOnchain.sol
@@ -193,3 +193,4 @@ contract PayPalOnchain is ReentrancyGuard {
 // Commit 68: docs: add deployment checklist
 // Commit 69: feat: add payment splitting functionality
 // Commit 70: refactor: optimize gas consumption
+// Commit 71: feat: add invoice template system
