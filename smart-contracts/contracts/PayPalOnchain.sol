4c67872562b0b04864e81d3be7e002bfc42b9352
Dev Jaja <jajak5242@gmail.com>
2026-01-30 23:13:03 -0500

    docs: add architecture overview

--- smart-contracts/contracts/PayPalOnchain.sol
+++ smart-contracts/contracts/PayPalOnchain.sol
@@ -210,3 +210,4 @@ contract PayPalOnchain is ReentrancyGuard {
 // Commit 85: refactor: improve function organization
 // Commit 86: feat: add automated refund system
 // Commit 87: security: add consensus mechanisms
+// Commit 88: docs: add architecture overview
