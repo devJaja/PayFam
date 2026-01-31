4d5cf3ff6e64909ae263a97248603d39226ce746
Dev Jaja <jajak5242@gmail.com>
2026-01-30 23:24:08 -0500

    docs: add merchant integration guide

--- smart-contracts/contracts/CommitHistory.sol
+++ smart-contracts/contracts/CommitHistory.sol
@@ -218,3 +218,4 @@ contract CommitHistory {
 // Commit 203: feat: implement merchant categories
 // Commit 204: security: add merchant encryption
 // Commit 205: refactor: optimize merchant compression
+// Commit 206: docs: add merchant integration guide
